7b663b55228a958db81aae2d0f8147ae6e1897f9
Güven Karabulut <56564068+guven07@users.noreply.github.com>
2023-01-01 22:33:35 +0300

    add new solutions (12,13,14,15,16,17)

---
+++ solutions/threeSumClosest.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function (nums, target) {
+    let closest = nums[0] + nums[1] + nums[2];
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length - 2; i++) {
+        let j = i + 1;
+        let k = nums.length - 1;
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k];
+            if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                closest = sum;
+            }
+            if (sum < target) {
+                j++;
+            } else {
+                k--;
+            }
+        }
+    }
+    return closest;
+};
